63b220529c4b5d52b8917611fa744ebedb94fb80
ching80411112 <ching80411112@gmail.com>
2019-11-10 23:34:35 +0800

    test1110

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
 // <!-- pageload -->
 
-$(window).load(function () {
-    $('.loadingbg').fadeOut(1000);
+// $(window).load(function () {
+//     $('.loadingbg').fadeOut(1000);
     
-    new Vivus('line_logo', {
-        // type: 'scenario-sync',
-        type: 'delayed',
-        duration: 100,
-        start: 'autostart',
-        dashGap: 100,
-        forceRender: false
-    },
-
-        //當以上結束後show原本隱藏的圖
-
-        function () {
-            $('#line_logo').delay(800).fadeOut();
-            $('#block_logo').delay(500).fadeIn();
-            
-
-            AOS.init({
-                duration: 1000
-            });
-        }
-    );
-    $('.active_slick').slick({
-        dots: false,
-        slidesToShow: 1,
-        infinite: false,
-        arrows: false,
-        fade: true,
-        autoplay: true,
-        autoplaySpeed: 3000
-    })
-});
-
-
     
+// });
+
+
+new Vivus('line_logo', {
+    // type: 'scenario-sync',
+    type: 'delayed',
+    duration: 100,
+    start: 'autostart',
+    dashGap: 100,
+    forceRender: false
+},
+
+    //當以上結束後show原本隱藏的圖
+
+    function () {
+        $('#line_logo').delay(800).fadeOut();
+        $('#block_logo').delay(500).fadeIn();
+        
+
+        AOS.init({
+            duration: 1000
+        });
+    }
+);
+$('.active_slick').slick({
+    dots: false,
+    slidesToShow: 1,
+    infinite: false,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 3000
+})    
 
 
 // gotop 
