1fede86333e612966974f4b54b52abc2138e836f
ching <ching.hearsttw@gmail.com>
2019-11-19 20:40:37 +0800

    update 第二區塊

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// fullpage
+var myFullpage = new fullpage('#fullpage', {
+    verticalCentered: true,
+    anchors: ['anchor1', 'anchor2', 'anchor3'],
+    menu: '#menu',
+    // sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE']
+});
+
+// 第二卡hover image
+document.documentElement.className="js";var supportsCssVars=function(){var e,t=document.createElement("style");return t.innerHTML="root: { --tmp-var: bold; }",document.head.appendChild(t),e=!!(window.CSS&&window.CSS.supports&&window.CSS.supports("font-weight","var(--tmp-var)")),t.parentNode.removeChild(t),e};supportsCssVars()||alert("Please view this demo in a modern browser that supports CSS Variables.");
